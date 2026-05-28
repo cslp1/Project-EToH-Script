0ec92a0913ee0781cd04d82deaa27d07c8a31a90
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-05-28 18:14:59 +0800

    Update ToM.lua

--- Games/EToH/Ring1/ToM.lua
+++ Games/EToH/Ring1/ToM.lua
@@ -2,6 +2,24 @@ return function()
     return {
         workspace.Towers.ToM.Obby:GetChildren()[78],
         workspace.Towers.ToM.Obby:GetChildren()[54],
+        workspace.Towers.ToM.Obby:GetChildren()[22],
+        workspace.Towers.ToM.Obby:GetChildren()[23],
+        workspace.Towers.ToM.Obby:GetChildren()[88],
+        workspace.Towers.ToM.Obby:GetChildren()[18],
+        workspace.Towers.ToM.Obby:GetChildren()[28],
+        workspace.Towers.ToM.Obby:GetChildren()[14],
+        workspace.Towers.ToM.Obby:GetChildren()[45],
+        workspace.Towers.ToM.Obby:GetChildren()[48],
+        workspace.Towers.ToM.Obby:GetChildren()[50],
+        workspace.Towers.ToM.Obby:GetChildren()[15],
+        workspace.Towers.ToM.Obby:GetChildren()[61],
+        workspace.Towers.ToM.Obby:GetChildren()[62],
+        workspace.Towers.ToM.Obby:GetChildren()[853],
+        workspace.Towers.ToM.Obby:GetChildren()[310],
+        workspace.Towers.ToM.Obby:GetChildren()[20],
+        workspace.Towers.ToM.Obby:GetChildren()[75],
+        workspace.Towers.ToM.Obby:GetChildren()[71],
+        workspace.Towers.ToM.Obby:GetChildren()[89]
         
     }
 end
