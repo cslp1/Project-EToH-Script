3b82c5110f9eb55d8e628c844a22dbe87024f5e0
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-05-31 16:29:07 +0800

    Update R1TR.lua

--- Games/EToH/Ring1/R1TR.lua
+++ Games/EToH/Ring1/R1TR.lua
@@ -1,6 +1,17 @@
 return function()
     return {
         "ToAST",
-    
+        "ToA",
+        "ToM",
+        "ToNI",
+        "ToH",
+        "ToK",
+        "ToKY",
+        "ToS",
+        "ToSP",
+        "ToR",
+        "ToIE",
+        "ToST",
+        "CoLS"
     }
 end
