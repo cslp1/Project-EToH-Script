5fdd609e1aea6b914b7d9b4de8c5eb8163342c7d
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-06-12 18:41:58 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,12 +1,3 @@
-getgenv().vex_key = "KEYLESS"
-
-local VexSDK = loadstring(game:HttpGet("https://vexbin.lol/vexlytics/sdk/main.lua"))()
-VexSDK.script_id = "e61cab62"
-
-local status = VexSDK.check_key(getgenv().vex_key)
-if status.code == "KEY_VALID" then
-    print("Welcome! Executions: " .. status.data.total_executions)
-    VexSDK.load_script()
-else
-    game.Players.LocalPlayer:Kick(status.message)
-end
+print("[Project EToH Script Loader] Step 1 : Loading Vexlytics")
+v=loadstring(game:HttpGet"https://vexbin.lol/vexlytics/sdk/main.lua")()v.script_id="e61cab62"if(v.check_key"KEYLESS").code=="KEY_VALID"then v.load_script()end
+print("[Project EToH Script Loader] Step 1 : Done ")
