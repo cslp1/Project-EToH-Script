63c6ebb1f00d151e6601087d3eeaa76708946dea
cslp1 <wrerfsdh5@gmail.com>
2026-06-20 17:19:05 +0200

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -106,7 +106,7 @@ return {
         { name = "CoSR",  category = "Ring6", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToIM",  category = "Ring6", suggestedTime = { min = "3", sec = "0" } },
         --Ring 7
-        { name = "FFAST",  category = "Ring7", suggestedTime = { min = "0", sec = "5" } },
+        { name = "FFAST",  category = "Ring7", suggestedTime = { min = "0", sec = "10" } },
         --Ring 9
         { name = "ToGO",  category = "Ring9", suggestedTime = { min = "3", sec = "0" } },
         { name = "ToBBB",  category = "Ring9", suggestedTime = { min = "3", sec = "0" } },
