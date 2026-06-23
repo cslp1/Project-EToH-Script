9c07f38e302c171160f8103c0f2cfeabe7013ab9
cslp1 <wrerfsdh5@gmail.com>
2026-06-23 13:56:32 +0200

    Update ToDAN.lua

--- Games/EToH/Ring0/ToDAN.lua
+++ Games/EToH/Ring0/ToDAN.lua
@@ -1455,6 +1455,5 @@ workspace.Towers["ToDAN"].Obby:GetChildren()[1046],
 workspace.Towers["ToDAN"].Obby:GetChildren()[1047],
 workspace.Towers["ToDAN"].Obby:GetChildren()[1049],
 workspace.Towers["ToDAN"].Obby:GetChildren()[1048],
-workspace.Towers["ToDAN"].Obby.WinPad,
             }
 end
