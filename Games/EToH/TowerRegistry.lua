e5cc7a3d38c4c1750a4b02213fcc72954444510e
cslp1 <wrerfsdh5@gmail.com>
2026-08-03 17:07:58 +0200

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -28,6 +28,7 @@ local Categories = {
     Zone10 = 9071004898,
     ["Pit-of-Misery"] = 15639952229,
     ElysianFields = 95241871632790,
+    TheDoghouse = 16976865869
     LegacyFortress = 116826560849733,
     ["100M-Event-Replay"] = 115856553162061,
 }
@@ -286,6 +287,8 @@ return {
         { name = "ToBF",  category = "Pit-of-Misery", suggestedTime = { min = "6", sec = "0" } },
         --Elysian  Fields
         { name = "SoGE",  category = "ElysianFields", suggestedTime = { min = "4", sec = "36" } },
+        --The Doghouse
+        { name = "MCoMC",  category = "TheDoghouse", suggestedTime = { min = "4", sec = "36" } },
         --Legacy Fortress
         { name = "ToC(L)",  category = "LegacyFortress", suggestedTime = { min = "4", sec = "36" } },
         { name = "ToUB(L)",  category = "LegacyFortress", suggestedTime = { min = "4", sec = "36" } },
