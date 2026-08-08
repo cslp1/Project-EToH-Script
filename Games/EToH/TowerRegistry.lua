a2f2c3b7cd00b35219d2c737b239d9bd6d628cda
cslp1 <wrerfsdh5@gmail.com>
2026-08-08 08:58:02 +0200

    Update TowerRegistry.lua

--- Games/EToH/TowerRegistry.lua
+++ Games/EToH/TowerRegistry.lua
@@ -349,7 +349,7 @@ return {
         { name = "ToTU(L)",  category = "LegacyFortress", suggestedTime = { min = "4", sec = "36" } },
         { name = "ToUP(L)",  category = "LegacyFortress", suggestedTime = { min = "4", sec = "36" } },
         { name = "ToWS(L)",  category = "LegacyFortress", suggestedTime = { min = "4", sec = "36" } },
-        --Legacy Fortress
+        --Heccfire Depths
         { name = "SoRP",  category = "HeccfireDepths", suggestedTime = { min = "4", sec = "36" } },
         --100M Event Replay
         { name = "100MC",  category = "100M-Event-Replay", suggestedTime = { min = "0", sec = "10" } },
