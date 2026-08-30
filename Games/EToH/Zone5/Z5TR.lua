4f0ebd08a67f35b99fa3cb535aee7b02541606f4
cslp1 <wrerfsdh5@gmail.com>
2026-08-30 10:33:19 +0200

    Create Z5TR.lua

---
+++ Games/EToH/Zone5/Z5TR.lua
@@ -0,0 +1,17 @@
+return function()
+    return {
+        "ToST",
+        "ToSSSS",
+        "ToBS",
+        "ToFV",
+        "ToSTS",
+        "ToUE",
+        "ToHH",
+        "ToHE",
+        "ToGT",
+        "ToAO",
+        "ToBBT",
+        "ToZA",
+        "CoCE",
+    }
+end
