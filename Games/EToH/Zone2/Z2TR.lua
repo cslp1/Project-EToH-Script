33a6e2de5a522bf6296aec26c1ce28aa85e05780
cslp1 <wrerfsdh5@gmail.com>
2026-09-04 20:21:46 +0200

    Create Z2TR.lua

---
+++ Games/EToH/Zone2/Z2TR.lua
@@ -0,0 +1,18 @@
+return function()
+    return {
+        "ToVR",
+        "ToSA",
+        "ToGLA",
+        "ToTA",
+        "ToUD",
+        "ToDaC",
+        "ToPF",
+        "ToSaS",
+        "ToEE",
+        "ToAZ",
+        "ToTT",
+        "ToGD",
+        "ToCM",
+        "CoGS",
+    }
+end
